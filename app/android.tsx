8a07ff655cf7c39b25bb307c9c561f86954d309b
infospectline <data.aidiagnostics@gmail.com>
2026-07-16 08:00:37 +0200

    Update login, signup, dashboard and mobile layout

--- app/android.tsx
+++ app/android.tsx
@@ -969,7 +969,7 @@ export default function AndroidBasic({
           left: 0,
           top: 0,
           width: "100vw",
-          height: "100svh",
+          height: "100dvh",
           opacity: hydrated ? 1 : 0,
           transition: "opacity 500ms ease",
         }}
